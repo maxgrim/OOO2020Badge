52755a3c911ad95b5527c946e2dc70fd24da6a76
maxgrim <ik@maxgrim.nl>
2020-10-29 10:37:47 +0100

    Added RGB module

---
+++ lib/Badge/src/RGB.cpp
@@ -0,0 +1,76 @@
+#include "RGB.h"
+#include "Badge.h"
+#include "Definitions.h"
+#include "Debug.h"
+
+#include <Adafruit_NeoPixel.h>
+
+static void handleBlinkLed();
+
+int blinkLed, blinkTimes, blinkTimesExecuted;
+uint32_t blinkColor;
+Task taskBlinkLed(150, TASK_FOREVER, &handleBlinkLed);
+
+static Adafruit_NeoPixel pixels = Adafruit_NeoPixel(RGB_N_LEDS, PIN_LED_RGB, NEO_GRB + NEO_KHZ800);
+
+static void handleBlinkLed()
+{
+    if (taskBlinkLed.getRunCounter() % 2 == 0)
+    {
+        pixels.clear();
+        pixels.setPixelColor(blinkLed, blinkColor);
+        pixels.show();
+    }
+    else
+    {
+        pixels.clear();
+        pixels.show();
+    }
+
+    if (++blinkTimesExecuted > (blinkTimes * 2))
+    {
+        taskBlinkLed.disable();
+    }
+}
+
+void rgbSetup()
+{
+    pixels.begin();
+    pixels.clear();
+    pixels.setBrightness(10);
+    pixels.show();
+
+    badgeTaskScheduler.addTask(taskBlinkLed);
+}
+
+void rgbLoop()
+{
+}
+
+void rgbSetSingleLed(int led, uint32_t color)
+{
+    pixels.clear();
+    pixels.setPixelColor(led, color);
+    pixels.show();
+}
+
+void rgbBlinkSingleLed(int led, int times, uint32_t color, TaskOnDisable doneCallback)
+{
+    blinkLed = led;
+    blinkTimes = times;
+    blinkColor = color;
+    blinkTimesExecuted = 0;
+
+    taskBlinkLed.setOnDisable(doneCallback);
+    taskBlinkLed.restart();
+}
+
+void rgbSetAllLeds(uint32_t color)
+{
+    for (uint16_t i = 0; i < pixels.numPixels(); i++)
+    {
+        pixels.setPixelColor(i, color);
+    }
+
+    pixels.show();
+}
---
+++ lib/Badge/src/RGB.h
@@ -0,0 +1,14 @@
+#ifndef RGB_H_SEEN
+#define RGB_H_SEEN
+
+#include <stdint.h>
+#include <TaskSchedulerDeclarations.h>
+
+void rgbSetup();
+void rgbLoop();
+
+void rgbSetSingleLed(int led, uint32_t color);
+void rgbBlinkSingleLed(int led, int times, uint32_t color, TaskOnDisable doneCallback);
+void rgbSetAllLeds(uint32_t color);
+
+#endif
