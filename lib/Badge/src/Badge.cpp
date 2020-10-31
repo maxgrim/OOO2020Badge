58a80ab77dfe4b8c668d39a0a436fc501d1a7c79
maxgrim <ik@maxgrim.nl>
2020-10-31 20:04:53 +0100

    Reformatting

--- lib/Badge/src/Badge.cpp
+++ lib/Badge/src/Badge.cpp
@@ -27,12 +27,14 @@ static bool buttonsPressed()
     return digitalRead(PIN_BUTTON_L) && digitalRead(PIN_BUTTON_R);
 }
 
-static void printWelcome() {
+static void printWelcome()
+{
     Serial.println(F("\r\n          .     .  .      +     .      .          .\r\n     .       .      .     #       .           .\r\n        .      .         ###            .      .      .\r\n      .      .   \"#:. .:##\"##:. .:#\"  .      .\r\n          .      . \"####\"###\"####\"  .\r\n       .     \"#:.    .:#\"###\"#:.    .:#\"  .        .       .\r\n  .             \"#########\"#########\"        .        .\r\n        .    \"#:.  \"####\"###\"####\"  .:#\"   .       .\r\n     .     .  \"#######\"\"##\"##\"\"#######\"                  .\r\n                .\"##\"#####\"#####\"##\"           .      .\r\n    .   \"#:. ...  .:##\"###\"###\"##:.  ... .:#\"     .\r\n      .     \"#######\"##\"#####\"##\"#######\"      .     .\r\n    .    .     \"#####\"\"#######\"\"#####\"    .      .    _==_ _\r\n            .     \"      000      \"    .     .      _,(\\\",)|_|\r\n       .         .   .   000     .        .          \\/. \\-|\r\n.. .. ..................O000O........................( :  )|...."));
     Serial.println(F("\r\n   ____  ________                _             \r\n  / __ \\/ __/ __/__  ____  _____(_)   _____     \r\n / / / / /_/ /_/ _ \\/ __ \\/ ___/ / | / / _ \\    \r\n/ /_/ / __/ __/  __/ / / (__  ) /| |/ /  __/    \r\n\\____/_/ /_/__\\___/_/ /_/____/_/ |___/\\___/     \r\n  / __ \\/ __/ __/____(_) /____                  \r\n / / / / /_/ /_/ ___/ / __/ _ \\                 \r\n/ /_/ / __/ __(__  ) / /_/  __/                 \r\n\\____/_/ /_/ /____/_/\\__/\\___/ __  _            \r\n  / __ \\____  ___  _________ _/ /_(_)___  ____  \r\n / / / / __ \\/ _ \\/ ___/ __ `/ __/ / __ \\/ __ \\ \r\n/ /_/ / /_/ /  __/ /  / /_/ / /_/ / /_/ / / / / \r\n\\____/ .___/\\___/_/   \\__,_/\\__/_/\\____/_/ /_/  \r\n   _/_/ ____ ___  ____                         \r\n  |__ \\/ __ \\__ \\/ __ \\                         \r\n  __/ / / / /_/ / / / /                         \r\n / __/ /_/ / __/ /_/ /                         \r\n/____|____/____|____/         Winter Edition    \r\n                                               \r\n"));
 }
 
-static void printEasyFlag() {
+static void printEasyFlag()
+{
     char flag[38];
     cryptoGetFlag(&flag[0], sizeof(flag), 64);
 
@@ -40,9 +42,13 @@ static void printEasyFlag() {
     Serial.printf("%s\r\n", flag);
 }
 
-void badgeFatalError(const char *message) {
+void badgeFatalError(const char *message)
+{
     Serial.println(message);
-    while(true) { yield(); };
+    while (true)
+    {
+        yield();
+    };
 }
 
 bool badgeSetup(size_t eepromSize, size_t eepromStartAddress)
@@ -60,7 +66,7 @@ bool badgeSetup(size_t eepromSize, size_t eepromStartAddress)
     Settings settings = settingsGetSettings();
 
     // Badge mode code
-    if(buttonsPressed()) 
+    if (buttonsPressed())
     {
         settings.badgeMode = settings.badgeMode == 0 ? 255 : 0;
         settingsSetSettings(settings);
