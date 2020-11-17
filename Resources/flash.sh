95ff3bbb73ae7a70e59d9204e79e600b48df79e6
maxgrim <ik@maxgrim.nl>
2020-11-17 16:07:30 +0100

    Updated flash.sh

--- Resources/flash.sh
+++ Resources/flash.sh
@@ -1,2 +1,8 @@
 #!/usr/bin/env bash
+# Usage: ./flash.sh <port> <firmware> <spiffs>
+esptool.py erase_flash
+
+echo -e "\r\nFlash erased, now put the badge in flash mode again"
+read -p "Press enter to continue..."
+
 esptool.py --chip esp8266 --port "$1" --baud 115200 write_flash 0 $2 1048576 $3
