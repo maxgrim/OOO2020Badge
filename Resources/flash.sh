69833e49994e8ec1f2779bb69fb7e146f611db2e
maxgrim <ik@maxgrim.nl>
2020-11-20 16:41:33 +0100

    Updated flash script and (hopefully) uploaded final firmware

--- Resources/flash.sh
+++ Resources/flash.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 # Usage: ./flash.sh <port> <firmware> <spiffs>
-esptool.py erase_flash
+esptool.py --chip esp8266 --port "$1" erase_flash
 
 echo -e "\r\nFlash erased, now put the badge in flash mode again"
 read -p "Press enter to continue..."
