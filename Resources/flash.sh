cd09434eefe8d256f6c61c4c457a01e41d40add5
maxgrim <ik@maxgrim.nl>
2020-11-25 19:32:27 +0100

    Increased flash speed for script

--- Resources/flash.sh
+++ Resources/flash.sh
@@ -5,4 +5,4 @@ esptool.py --chip esp8266 --port "$1" erase_flash
 echo -e "\r\nFlash erased, now put the badge in flash mode again"
 read -p "Press enter to continue..."
 
-esptool.py --chip esp8266 --port "$1" --baud 115200 write_flash 0 $2 1048576 $3
+esptool.py --chip esp8266 --port "$1" --baud 460800 write_flash --verify 0 $2 1048576 $3
